b445a59fbac883ac209d3bf717cf8b46b416fb11
sadeshwar94 <sadeshwarnakulasooriya@gmail.com>
2022-03-09 13:52:16 +0900

    Action Section and Input Button section

--- pages/index.js
+++ pages/index.js
@@ -19,21 +19,36 @@ export default function Home() {
             <UserImage src="https://lh3.googleusercontent.com/ogw/ADea4I6R5ml8aXasbQmd64TJAhczxmASNE1OCjXj9EZ2=s32-c-mo" />
           </Profile>
         </Header>
-
         {/* ActionButtons */}
+        <ActionButtons>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/cyvcpfF/uberx.png" />
+            Ride
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/n776JLm/bike.png" />
+            Wheels
+          </ActionButton>
+          <ActionButton>
+            <ActionButtonImage src="https://i.ibb.co/5RjchBg/uberschedule.png" />
+            Reserve
+          </ActionButton>
+        </ActionButtons>
+
         {/* InputButtons */}
+        <InputButton> Where to ? </InputButton>
       </ActionItems>
     </Wrapper>
   );
 }
 
 const Wrapper = tw.div`
-flex flex-col bg-red-300 h-screen
+flex flex-col h-screen
 
 `;
 
 const ActionItems = tw.div`
-flex-1 
+flex-1 p-4 
 `;
 
 const Header = tw.div`
@@ -54,4 +69,20 @@ mr-4 w-20 text-sm
 
 const UserImage = tw.img`
 h-12 w-12 rounded-full border border-gray-200 p-px
+`;
+
+const ActionButtons = tw.div`
+flex 
+`;
+
+const ActionButton = tw.div`
+flex bg-gray-200  flex-1 m-1 h-32 items-center flex-col justify-center rounded-lg transform hover:scale-105 transition text-xl
+`;
+
+const ActionButtonImage = tw.img`
+h-3/5
+`;
+
+const InputButton = tw.div`
+
 `;
