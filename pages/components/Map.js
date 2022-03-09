260f25b137d10fa79668f50a178f6728d43ce622
sadeshwar94 <sadeshwarnakulasooriya@gmail.com>
2022-03-09 11:17:18 +0900

    4 files modified, 3 added

---
+++ pages/components/Map.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import tw from 'tailwind-styled-components';
+import mapboxgl from 'mapbox-gl';
+import { useEffect } from 'react';
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic2FkZXNod2FyIiwiYSI6ImNpeTUwcnNuMzAwNDQzM3FoMnRsdG94dWUifQ.aK-Xih6Lfrglfqwa9n6Z1A';
+
+const Map = () => {
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [135.5375, 34.794],
+      zoom: 11.1,
+    });
+  });
+  return <Wrapper id="map"></Wrapper>;
+};
+
+export default Map;
+
+const Wrapper = tw.div`
+flex-1
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,57 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import tw from 'tailwind-styled-components';
+import Map from './components/Map';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Wrapper>
+      <Map />
+      <ActionItems>
+        {/* Header */}
+        <Header>
+          <UberLogo src="https://d1a3f4spazzrp4.cloudfront.net/uberex/duc/images/logos/Uber_Logotype_Digital_black.png" />
 
+          <Profile>
+            <Name>Sadesh War </Name>
+            <UserImage src="https://lh3.googleusercontent.com/ogw/ADea4I6R5ml8aXasbQmd64TJAhczxmASNE1OCjXj9EZ2=s32-c-mo" />
+          </Profile>
+        </Header>
 
-     
-    </div>
+        {/* ActionButtons */}
+        {/* InputButtons */}
+      </ActionItems>
+    </Wrapper>
   );
 }
+
+const Wrapper = tw.div`
+flex flex-col bg-red-300 h-screen
+
+`;
+
+const ActionItems = tw.div`
+flex-1 
+`;
+
+const Header = tw.div`
+flex justify-between items-center
+`;
+
+const UberLogo = tw.img`
+h-28
+`;
+
+const Profile = tw.div`
+flex items-center 
+`;
+
+const Name = tw.div`
+mr-4 w-20 text-sm
+`;
+
+const UserImage = tw.img`
+h-12 w-12 rounded-full border border-gray-200 p-px
+`;
