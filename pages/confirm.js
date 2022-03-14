080339f161f52fa3d1c05bf2f454d611b3fa4b07
sadeshwar94 <sadeshwarnakulasooriya@gmail.com>
2022-03-14 11:29:31 +0900

    getCoordinates-to-Log-Console

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import { useEffect } from 'react';
 import tw from 'tailwind-styled-components';
 import Map from './components/Map';
 
 const confirm = () => {
+  const getCoordinates = () => {
+    const location = 'Suita Osaka';
+    //JS fetch method
+    fetch(
+      'https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?' +
+        new URLSearchParams({
+          access_token:
+            'pk.eyJ1Ijoic2FkZXNod2FyIiwiYSI6ImNpeTUwcnNuMzAwNDQzM3FoMnRsdG94dWUifQ.aK-Xih6Lfrglfqwa9n6Z1A',
+          limit: 1,
+        })
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  useEffect(() => {
+    getCoordinates();
+  }, []);
+
   return (
     <Wrapper>
       <Map />
